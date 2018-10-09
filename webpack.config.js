da407557ec42e798f624771547cfdf68c4b63185
Saneef Ansari <hello@saneef.com>
2018-10-09 20:56:00 +0530

    build(webpack): minify css on production

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,52 @@
-const postcssPresetEnv = require('postcss-preset-env');
-const postcssImport = require("postcss-import")
+const postcssPresetEnv = require("postcss-preset-env");
+const postcssImport = require("postcss-import");
+const cssnano = require("cssnano");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+module.exports = (env, argv) => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: "css-loader", options: { importLoaders: 1 } },
+            {
+              loader: "postcss-loader",
+              options: {
+                ident: "postcss",
+                plugins: () => {
+                  const plugins = [
+                    postcssImport(),
+                    postcssPresetEnv({
+                      stage: 0
+                    })
+                  ]
+
+                  if (argv.mode === 'production') {
+                    plugins.push(cssnano());
+                  }
+
+                  return plugins;
+                }
+              }
+            }
+          ]
         }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          { loader: 'postcss-loader', options: {
-            ident: 'postcss',
-            plugins: () => [
-              postcssImport(),
-              postcssPresetEnv({
-                stage: 0
-              })
-            ]
-          } }
-        ]
-      }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "main.css"
+      })
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "main.css"
-    })
-  ]
-}
+  };
+};
